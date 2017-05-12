e5805573ba02ea992665996984ce11cd6b5ee234
App Academy Student <student@appacademy.io>
2017-05-11 17:54:37 -0700

    loop music

--- PokeDex/frontend/pokedex.jsx
+++ PokeDex/frontend/pokedex.jsx
@@ -4,14 +4,7 @@ import * as APIUtil from './util/api_util';
 import configureStore from './store/store';
 import Root from './root';
 import { HashRouter, Route } from 'react-router-dom';
-import { receiveAllPokemon, requestAllPokemon } from './actions/pokemon_actions';
-import { selectAllPokemon } from './reducers/selectors';
 
-
-window.receiveAllPokemon = receiveAllPokemon;
-window.fetchAllPokemon = APIUtil.fetchAllPokemon;
-window.requestAllPokemon = requestAllPokemon;
-window.selectAllPokemon = selectAllPokemon;
 
 document.addEventListener('DOMContentLoaded', () => {
   var song = document.getElementById("yes-audio");
@@ -26,6 +19,5 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   const root = document.getElementById('root');
   const store = configureStore();
-  window.store = store;
   ReactDOM.render(<Root store={store} />, root);
 });
